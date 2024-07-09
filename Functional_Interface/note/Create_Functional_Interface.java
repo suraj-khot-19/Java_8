2fb4658487d25ba26590d2f3381e403941065175
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-07-09 07:55:30 +0530

    functional interface creating in java

---
+++ Functional_Interface/note/Create_Functional_Interface.java
@@ -0,0 +1,12 @@
+package Functional_Interface.note;
+
+@FunctionalInterface
+interface MyFunctionalInterface{
+    void getName();
+    ///  it's having only one abstract method
+}
+public class Create_Functional_Interface {
+    public static void main(String[] args) {
+
+    }
+}
