f02175b804dc0824ff7070ab4f38a9c4b989aca8
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-07-16 07:55:30 +0530

    Not a functional Interface

---
+++ Functional_Interface/note/Not_Functional_Interface.java
@@ -0,0 +1,16 @@
+package Functional_Interface.note;
+
+/// Multiple non-overriding abstract methods found in interface Functional_Interface. note. NotFunctionalInterface
+
+//@FunctionalInterface
+interface NotFunctionalInterface{
+    void getName();
+
+    void getAge();
+}
+
+public class Not_Functional_Interface {
+    public static void main(String[] args) {
+
+    }
+}
