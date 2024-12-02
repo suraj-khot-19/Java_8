ac7d7b0b44bd1680a2f819a215eee980e40d5f18
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-12-02 07:55:30 +0530

    functional Interface usage and with static, default method

---
+++ Functional_Interface/note/Functional_Interface_Usage.java
@@ -0,0 +1,29 @@
+package Functional_Interface.note;
+
+@FunctionalInterface
+interface MyFunctionalInterfaceUsage{
+    void getName();
+
+    static int getAge(){
+        return 24;
+    }
+
+    default int getSalary(){
+        return 24000;
+    }
+}
+
+public class Functional_Interface_Usage {
+    public static void main(String[] args) {
+        MyFunctionalInterfaceUsage ref = () -> {
+            System.out.println("Suraj");
+        };
+
+        ref.getName();
+
+        MyFunctionalInterfaceUsage.getAge();
+
+        int sal = ref.getSalary();
+        System.out.println(sal);
+    }
+}
