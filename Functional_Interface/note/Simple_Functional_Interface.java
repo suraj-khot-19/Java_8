6a34c71b0874ba956fd291f727395339fbd88fee
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-12-03 07:55:30 +0530

    functional Interface usage

---
+++ Functional_Interface/note/Simple_Functional_Interface.java
@@ -0,0 +1,13 @@
+package Functional_Interface.note;
+
+@FunctionalInterface
+interface A{
+    int getData();
+}
+public class Simple_Functional_Interface {
+    public static void main(String[] args) {
+        A ref=()->5;
+        int x=ref.getData();
+        System.out.println(x);
+    }
+}
