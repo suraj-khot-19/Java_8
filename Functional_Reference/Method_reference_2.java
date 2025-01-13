398915e35657f73a4eceefb427c82d46102dad0b
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-13 17:36:34 +0530

    method reference 2

---
+++ Functional_Reference/Method_reference_2.java
@@ -0,0 +1,12 @@
+package Functional_Reference;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Method_reference_2 {
+    public static void main(String[] args) {
+        List<String> name= Arrays.asList("suraj","sohan","adaya");
+
+        name.forEach(System.out::println); //  :: represents method reference
+    }
+}
