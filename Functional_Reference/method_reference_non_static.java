85ec4934b897c54242b00acdbbb81228a8d358f5
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-13 17:43:57 +0530

    method reference 3

---
+++ Functional_Reference/method_reference_non_static.java
@@ -0,0 +1,22 @@
+package Functional_Reference;
+
+import java.util.Arrays;
+import java.util.List;
+
+class A {
+    void Print(int x) {
+        System.out.println(x);
+    }
+}
+
+public class method_reference_non_static {
+    public static void main(String[] args) {
+        A obj=new A();
+
+        List<Integer> list= Arrays.asList(1,2,4,3,1,2);
+
+        //list.forEach(A::Print); // Non-static method cannot be referenced from a static context
+
+        list.forEach(obj::Print);
+    }
+}
