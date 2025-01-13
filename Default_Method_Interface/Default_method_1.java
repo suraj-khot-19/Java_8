417e046445c42a78ffe57abd51bacd75b52e0390
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-13 14:10:34 +0530

    Default interface

---
+++ Default_Method_Interface/Default_method_1.java
@@ -0,0 +1,16 @@
+package Default_Method_Interface;
+
+interface parent{
+    default void sayHello(){ // default means it having a body
+        System.out.println("Interface saying hello!");
+    }
+}
+class child implements parent{
+
+}
+public class Default_method_1 {
+    public static void main(String[] args) {
+        child obj=new child();
+        obj.sayHello();
+    }
+}
